726c2f045c059a914c24b7d72147f5699cad3544
Mark Probst <mark.probst@gmail.com>
2024-12-26 13:55:10 +0100

    Handle block comments

--- src/parse-imports.ts
+++ src/parse-imports.ts
@@ -12,30 +12,43 @@ export interface Import {
 export type Part = string | Import;
 type Parts = readonly Part[];
 
+const commentRegexp = /\/\*(\*(?!\/)|[^*])*\*\//g;
 const importRegexp =
     /(import|export)(\s+type)?\s+(\*|{([^}]*?)}|[a-zA-Z_$][a-zA-Z\d_$]*)\s+from "([^";]+)";/g;
 
 export function parseImports(code: string): Parts {
-    const parsedParts = separateStringByMatches(importRegexp, code);
     const parts: Part[] = [];
 
-    for (const p of parsedParts) {
-        if (typeof p === "string") {
-            parts.push(p);
-        } else {
-            const kind = p[1] as "import" | "export";
-            const type = p[2] !== undefined;
-            const path = p[5];
-            if (p[3] === "*") {
-                parts.push({ kind, isType: type, names: true, path });
-            } else if (p[4]) {
-                const names = p[4]
-                    .split(",")
-                    .map((n) => n.trim())
-                    .filter((n) => n.length > 0);
-                parts.push({ kind, isType: type, names, path });
+    // First we find all the block comments
+    const commentParts = separateStringByMatches(commentRegexp, code);
+    for (const c of commentParts) {
+        if (typeof c !== "string") {
+            // It's a comment, so we just push it
+            parts.push(c[0]);
+            continue;
+        }
+
+        // It's non-comment code, so we parse it
+        const parsedParts = separateStringByMatches(importRegexp, c);
+
+        for (const p of parsedParts) {
+            if (typeof p === "string") {
+                parts.push(p);
             } else {
-                parts.push(p[0]);
+                const kind = p[1] as "import" | "export";
+                const type = p[2] !== undefined;
+                const path = p[5];
+                if (p[3] === "*") {
+                    parts.push({ kind, isType: type, names: true, path });
+                } else if (p[4]) {
+                    const names = p[4]
+                        .split(",")
+                        .map((n) => n.trim())
+                        .filter((n) => n.length > 0);
+                    parts.push({ kind, isType: type, names, path });
+                } else {
+                    parts.push(p[0]);
+                }
             }
         }
     }
